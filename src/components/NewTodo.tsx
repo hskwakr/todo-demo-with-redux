61dcbb84190149d75b9b4d8a84a1219e59d3cca3
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-02-06 03:55:38 +0900

    Update NewTodo.tsx

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,9 +1,5 @@
-const NewTodo = () => {
-  return (
-    <>
-      <input type="text" placeholder="Enter TODO..." aria-label="new todo" />
-    </>
-  );
-};
+const NewTodo = () => (
+  <input type="text" placeholder="Enter TODO..." aria-label="new todo" />
+);
 
 export default NewTodo;
