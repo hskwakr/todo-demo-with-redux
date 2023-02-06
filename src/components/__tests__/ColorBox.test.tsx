059a6118c92d8db4c502dfdf2217b26262af2101
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-02-07 00:30:10 +0900

    Add ColorBox component

---
+++ src/components/__tests__/ColorBox.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import ColorBox from '../ColorBox';
+
+describe('ColorBox Component Appearance', () => {
+  test('It has a prop to specify background color', () => {
+    render(<ColorBox color="red" />);
+
+    const box = screen.getByRole('generic', { name: /color box/ });
+    expect(box).toHaveStyle('background-color: red');
+  });
+});
