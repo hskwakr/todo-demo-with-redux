34e2ba9e459f134aaf98bb54ba89f6cc42c7b641
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-02-06 23:31:17 +0900

    Update index.tsx
    Add css normalizer by material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CssBaseline } from '@mui/material';
 import App from './App';
 
 import './index.css';
@@ -11,8 +12,10 @@ import '@fontsource/roboto/700.css';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
