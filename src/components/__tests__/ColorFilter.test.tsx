185cefda5fa549bbf91c3350260fa45a6ce02e89
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-02-07 08:50:02 +0900

    Update ColorFilter.test.tsx

--- src/components/__tests__/ColorFilter.test.tsx
+++ src/components/__tests__/ColorFilter.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ColorFilter from '../ColorFilter';
 import { colors } from '../../utils/color';
 
@@ -8,5 +9,19 @@ describe('ColorFilter Component Appearance', () => {
 
     const buttons = await screen.findAllByRole('button');
     expect(buttons.length).toBe(colors.length);
+  });
+});
+
+describe('ColorFilter Component Behavior', () => {
+  test('The clicked button turns disable', async () => {
+    const user = userEvent.setup();
+    render(<ColorFilter />);
+
+    const buttons = await screen.findAllByRole('button');
+
+    const target = buttons[0];
+    expect(target).not.toHaveAttribute('disabled');
+    await user.click(target);
+    expect(target).toHaveAttribute('disabled');
   });
 });
