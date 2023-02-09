3ec8ffd515d3da3cd7d5cdb80449bb76ec1e5f7a
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-02-10 06:04:57 +0900

    Add filtersSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todosReducer from './todosSlice';
+import filtersReducer from './filtersSlice';
 
 const store = configureStore({
   reducer: {
     todos: todosReducer,
+    filters: filtersReducer,
   },
 });
 
