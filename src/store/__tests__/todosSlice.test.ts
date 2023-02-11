c1c990ed4e9b9b8840805e5221d9393a7669d650
akira <33633391+hskwakr@users.noreply.github.com>
2023-02-12 02:02:19 +0900

    Todo slice actions (#35)
    * Add todoAdded action
    
    * Add todoDeleted action
    
    * Add todoToggled action
    
    * Add todoColorChanged action
    
    * Add manyTodosDeleted action
    
    * Add manyTodosToggled action

--- src/store/__tests__/todosSlice.test.ts
+++ src/store/__tests__/todosSlice.test.ts
@@ -1,4 +1,11 @@
-import reducer from '../todosSlice';
+import reducer, {
+  manyTodosDeleted,
+  manyTodosToggled,
+  todoAdded,
+  todoColorChanged,
+  todoDeleted,
+  todoToggled,
+} from '../todosSlice';
 
 describe('todosSlice reducer', () => {
   test('It returns the initial state', () => {
@@ -38,6 +45,498 @@ describe('todosSlice reducer', () => {
       },
       { type: undefined }
     );
+    expect(got).toEqual(want);
+  });
+});
+
+describe('todosSlice action', () => {
+  test('todoAdded add a todo to empty list', () => {
+    const want = {
+      entities: {
+        '0': {
+          id: '0',
+          name: 'Test something',
+          color: 'gray',
+          completed: false,
+        },
+      },
+      ids: ['0'],
+    };
+
+    const got = reducer(undefined, todoAdded('Test something', 'gray'));
+    expect(got).toEqual(want);
+  });
+
+  test('todoAdded add a todo to existing list', () => {
+    const want = {
+      entities: {
+        '0': {
+          id: '0',
+          name: 'Do something',
+          color: 'gray',
+          completed: false,
+        },
+        '1': {
+          id: '1',
+          name: 'Test something',
+          color: 'red',
+          completed: false,
+        },
+      },
+      ids: ['0', '1'],
+    };
+
+    const got = reducer(
+      {
+        entities: {
+          '0': {
+            id: '0',
+            name: 'Do something',
+            color: 'gray',
+            completed: false,
+          },
+        },
+        ids: ['0'],
+      },
+      todoAdded('Test something', 'red')
+    );
+
+    expect(got).toEqual(want);
+  });
+
+  test('todoDeleted remove a last todo from existing list', () => {
+    const want = {
+      entities: {
+        '0': {
+          id: '0',
+          name: 'Test something 0',
+          color: 'gray',
+          completed: false,
+        },
+        '1': {
+          id: '1',
+          name: 'Do something 1',
+          color: 'red',
+          completed: false,
+        },
+      },
+      ids: ['0', '1'],
+    };
+
+    const got = reducer(
+      {
+        entities: {
+          '0': {
+            id: '0',
+            name: 'Test something 0',
+            color: 'gray',
+            completed: false,
+          },
+          '1': {
+            id: '1',
+            name: 'Do something 1',
+            color: 'red',
+            completed: false,
+          },
+          '2': {
+            id: '2',
+            name: 'Do something 2',
+            color: 'red',
+            completed: false,
+          },
+        },
+        ids: ['0', '1', '2'],
+      },
+      todoDeleted('2')
+    );
+
+    expect(got).toEqual(want);
+  });
+
+  test('todoDeleted remove a first todo from existing list', () => {
+    const want = {
+      entities: {
+        '1': {
+          id: '1',
+          name: 'Do something 1',
+          color: 'red',
+          completed: false,
+        },
+        '2': {
+          id: '2',
+          name: 'Do something 2',
+          color: 'red',
+          completed: false,
+        },
+      },
+      ids: ['1', '2'],
+    };
+
+    const got = reducer(
+      {
+        entities: {
+          '0': {
+            id: '0',
+            name: 'Test something 0',
+            color: 'gray',
+            completed: false,
+          },
+          '1': {
+            id: '1',
+            name: 'Do something 1',
+            color: 'red',
+            completed: false,
+          },
+          '2': {
+            id: '2',
+            name: 'Do something 2',
+            color: 'red',
+            completed: false,
+          },
+        },
+        ids: ['0', '1', '2'],
+      },
+      todoDeleted('0')
+    );
+
+    expect(got).toEqual(want);
+  });
+
+  test('todoDeleted remove a middle todo from existing list', () => {
+    const want = {
+      entities: {
+        '0': {
+          id: '0',
+          name: 'Test something 0',
+          color: 'gray',
+          completed: false,
+        },
+        '2': {
+          id: '2',
+          name: 'Do something 2',
+          color: 'red',
+          completed: false,
+        },
+      },
+      ids: ['0', '2'],
+    };
+
+    const got = reducer(
+      {
+        entities: {
+          '0': {
+            id: '0',
+            name: 'Test something 0',
+            color: 'gray',
+            completed: false,
+          },
+          '1': {
+            id: '1',
+            name: 'Do something 1',
+            color: 'red',
+            completed: false,
+          },
+          '2': {
+            id: '2',
+            name: 'Do something 2',
+            color: 'red',
+            completed: false,
+          },
+        },
+        ids: ['0', '1', '2'],
+      },
+      todoDeleted('1')
+    );
+
+    expect(got).toEqual(want);
+  });
+
+  test('todoToggled toggle completed prop from false to true', () => {
+    const want = {
+      entities: {
+        '0': {
+          id: '0',
+          name: 'Test something',
+          color: 'gray',
+          completed: true,
+        },
+      },
+      ids: ['0'],
+    };
+
+    const got = reducer(
+      {
+        entities: {
+          '0': {
+            id: '0',
+            name: 'Test something',
+            color: 'gray',
+            completed: false,
+          },
+        },
+        ids: ['0'],
+      },
+      todoToggled('0')
+    );
+
+    expect(got).toEqual(want);
+  });
+
+  test('todoToggled toggle completed prop from true to false', () => {
+    const want = {
+      entities: {
+        '0': {
+          id: '0',
+          name: 'Test something',
+          color: 'gray',
+          completed: false,
+        },
+      },
+      ids: ['0'],
+    };
+
+    const got = reducer(
+      {
+        entities: {
+          '0': {
+            id: '0',
+            name: 'Test something',
+            color: 'gray',
+            completed: true,
+          },
+        },
+        ids: ['0'],
+      },
+      todoToggled('0')
+    );
+
+    expect(got).toEqual(want);
+  });
+
+  test('todoColorChanged change color prop', () => {
+    const want = {
+      entities: {
+        '0': {
+          id: '0',
+          name: 'Test something',
+          color: 'red',
+          completed: false,
+        },
+      },
+      ids: ['0'],
+    };
+
+    const got = reducer(
+      {
+        entities: {
+          '0': {
+            id: '0',
+            name: 'Test something',
+            color: 'gray',
+            completed: false,
+          },
+        },
+        ids: ['0'],
+      },
+      todoColorChanged('0', 'red')
+    );
+
+    expect(got).toEqual(want);
+  });
+
+  test('manyTodosDeleted deletes last two todos from list', () => {
+    const want = {
+      entities: {
+        '0': {
+          id: '0',
+          name: 'Test something 0',
+          color: 'gray',
+          completed: false,
+        },
+      },
+      ids: ['0'],
+    };
+
+    const got = reducer(
+      {
+        entities: {
+          '0': {
+            id: '0',
+            name: 'Test something 0',
+            color: 'gray',
+            completed: false,
+          },
+          '1': {
+            id: '1',
+            name: 'Test something 1',
+            color: 'gray',
+            completed: false,
+          },
+          '2': {
+            id: '2',
+            name: 'Test something 2',
+            color: 'gray',
+            completed: false,
+          },
+        },
+        ids: ['0', '1', '2'],
+      },
+      manyTodosDeleted(['1', '2'])
+    );
+
+    expect(got).toEqual(want);
+  });
+
+  test('manyTodosDeleted deletes first two todos from list', () => {
+    const want = {
+      entities: {
+        '2': {
+          id: '2',
+          name: 'Test something 2',
+          color: 'gray',
+          completed: false,
+        },
+      },
+      ids: ['2'],
+    };
+
+    const got = reducer(
+      {
+        entities: {
+          '0': {
+            id: '0',
+            name: 'Test something 0',
+            color: 'gray',
+            completed: false,
+          },
+          '1': {
+            id: '1',
+            name: 'Test something 1',
+            color: 'gray',
+            completed: false,
+          },
+          '2': {
+            id: '2',
+            name: 'Test something 2',
+            color: 'gray',
+            completed: false,
+          },
+        },
+        ids: ['0', '1', '2'],
+      },
+      manyTodosDeleted(['0', '1'])
+    );
+
+    expect(got).toEqual(want);
+  });
+
+  test('manyTodosToggled toggles first two todos to true in the list', () => {
+    const want = {
+      entities: {
+        '0': {
+          id: '0',
+          name: 'Test something 0',
+          color: 'gray',
+          completed: true,
+        },
+        '1': {
+          id: '1',
+          name: 'Test something 1',
+          color: 'gray',
+          completed: true,
+        },
+        '2': {
+          id: '2',
+          name: 'Test something 2',
+          color: 'gray',
+          completed: false,
+        },
+      },
+      ids: ['0', '1', '2'],
+    };
+
+    const got = reducer(
+      {
+        entities: {
+          '0': {
+            id: '0',
+            name: 'Test something 0',
+            color: 'gray',
+            completed: false,
+          },
+          '1': {
+            id: '1',
+            name: 'Test something 1',
+            color: 'gray',
+            completed: true,
+          },
+          '2': {
+            id: '2',
+            name: 'Test something 2',
+            color: 'gray',
+            completed: false,
+          },
+        },
+        ids: ['0', '1', '2'],
+      },
+      manyTodosToggled(['0', '1'], true)
+    );
+
+    expect(got).toEqual(want);
+  });
+
+  test('manyTodosToggled toggles last two todos to false in the list', () => {
+    const want = {
+      entities: {
+        '0': {
+          id: '0',
+          name: 'Test something 0',
+          color: 'gray',
+          completed: false,
+        },
+        '1': {
+          id: '1',
+          name: 'Test something 1',
+          color: 'gray',
+          completed: false,
+        },
+        '2': {
+          id: '2',
+          name: 'Test something 2',
+          color: 'gray',
+          completed: false,
+        },
+      },
+      ids: ['0', '1', '2'],
+    };
+
+    const got = reducer(
+      {
+        entities: {
+          '0': {
+            id: '0',
+            name: 'Test something 0',
+            color: 'gray',
+            completed: false,
+          },
+          '1': {
+            id: '1',
+            name: 'Test something 1',
+            color: 'gray',
+            completed: true,
+          },
+          '2': {
+            id: '2',
+            name: 'Test something 2',
+            color: 'gray',
+            completed: false,
+          },
+        },
+        ids: ['0', '1', '2'],
+      },
+      manyTodosToggled(['1', '2'], false)
+    );
+
     expect(got).toEqual(want);
   });
 });
--- src/store/todosSlice.ts
+++ src/store/todosSlice.ts
@@ -1,5 +1,10 @@
-import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import {
+  PayloadAction,
+  createEntityAdapter,
+  createSlice,
+} from '@reduxjs/toolkit';
 import { TodoType } from '../utils/todo';
+import { ColorType } from '../utils/color';
 
 const todosAdapter = createEntityAdapter<TodoType>();
 const initialState = todosAdapter.getInitialState();
@@ -7,7 +12,118 @@ const initialState = todosAdapter.getInitialState();
 const todosSlice = createSlice({
   name: 'todos',
   initialState,
-  reducers: {},
+  reducers: {
+    todoAdded: {
+      reducer(
+        state,
+        action: PayloadAction<{ name: string; color: ColorType }>
+      ) {
+        const { name, color } = action.payload;
+
+        const todo: TodoType = {
+          id: state.ids.length.toString(),
+          name,
+          color,
+          completed: false,
+        };
+
+        todosAdapter.addOne(state, todo);
+      },
+      prepare(name: string, color: ColorType) {
+        return {
+          payload: { name, color },
+        };
+      },
+    },
+
+    todoDeleted: {
+      reducer(state, action: PayloadAction<string>) {
+        const id = action.payload;
+        todosAdapter.removeOne(state, id);
+      },
+      prepare(id: string) {
+        return {
+          payload: id,
+        };
+      },
+    },
+
+    todoToggled: {
+      reducer(state, action: PayloadAction<string>) {
+        const id = action.payload;
+        const todo = state.entities[id];
+
+        if (todo) {
+          todo.completed = !todo.completed;
+        }
+      },
+      prepare(id: string) {
+        return {
+          payload: id,
+        };
+      },
+    },
+
+    todoColorChanged: {
+      reducer(state, action: PayloadAction<{ id: string; color: ColorType }>) {
+        const { id, color } = action.payload;
+        const todo = state.entities[id];
+
+        if (todo) {
+          todo.color = color;
+        }
+      },
+      prepare(id: string, color: ColorType) {
+        return {
+          payload: {
+            id,
+            color,
+          },
+        };
+      },
+    },
+
+    manyTodosDeleted: {
+      reducer(state, action: PayloadAction<string[]>) {
+        const ids = action.payload;
+        todosAdapter.removeMany(state, ids);
+      },
+      prepare(ids: string[]) {
+        return {
+          payload: ids,
+        };
+      },
+    },
+
+    manyTodosToggled: {
+      reducer(
+        state,
+        action: PayloadAction<{ ids: string[]; completed: boolean }>
+      ) {
+        const { ids, completed } = action.payload;
+        const updates = ids.map(id => ({
+          id,
+          changes: { completed },
+        }));
+
+        todosAdapter.updateMany(state, updates);
+      },
+      prepare(ids: string[], completed: boolean) {
+        return {
+          payload: { ids, completed },
+        };
+      },
+    },
+  },
 });
 
 export default todosSlice.reducer;
+
+export const {
+  todoAdded,
+  todoDeleted,
+  todoToggled,
+  todoColorChanged,
+  manyTodosDeleted,
+  manyTodosToggled,
+} = todosSlice.actions;
