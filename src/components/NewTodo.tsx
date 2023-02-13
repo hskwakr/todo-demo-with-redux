99d0cfcb366bfca735e6c981fdd52fb25e4cedeb
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-02-14 04:19:05 +0900

    Update NewTodo.tsx

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,5 +1,27 @@
-import { Input } from '@mui/material';
+import { useState } from 'react';
+import { Grid, Input } from '@mui/material';
+import ColorSwitch from './ColorSwitch';
+import { ColorType } from '../utils/color';
 
-const NewTodo = () => <Input placeholder="Enter TODO..." />;
+const NewTodo = () => {
+  const [color, setColor] = useState<ColorType>('gray');
+
+  return (
+    <Grid container alignItems="center">
+      <Grid item>
+        <ColorSwitch
+          initColor={color}
+          onColorUpdated={c => {
+            setColor(c);
+          }}
+        />
+      </Grid>
+
+      <Grid item>
+        <Input placeholder="Enter TODO..." />
+      </Grid>
+    </Grid>
+  );
+};
 
 export default NewTodo;
