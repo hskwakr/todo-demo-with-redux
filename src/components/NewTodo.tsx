bed6672616d90aa3a63a8e236390bd7fdb5b6b09
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-02-14 05:57:32 +0900

    Update NewTodo.tsx

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -5,6 +5,7 @@ import { ColorType } from '../utils/color';
 
 const NewTodo = () => {
   const [color, setColor] = useState<ColorType>('gray');
+  const [text, setText] = useState('');
 
   return (
     <Grid container alignItems="center">
@@ -18,7 +19,20 @@ const NewTodo = () => {
       </Grid>
 
       <Grid item>
-        <Input placeholder="Enter TODO..." />
+        <Input
+          placeholder="Enter TODO..."
+          value={text}
+          onChange={e => {
+            setText(e.target.value);
+          }}
+          onKeyDown={e => {
+            const trimmed = e.currentTarget.value.trim();
+
+            if (e.key === 'Enter' && trimmed) {
+              setText('');
+            }
+          }}
+        />
       </Grid>
     </Grid>
   );
