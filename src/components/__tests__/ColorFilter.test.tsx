9864b66fddd56bf8d9ef6e627b1dc70c21442268
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-02-13 23:32:43 +0900

    Update ColorFilter.tsx

--- src/components/__tests__/ColorFilter.test.tsx
+++ src/components/__tests__/ColorFilter.test.tsx
@@ -4,41 +4,25 @@ import ColorFilter from '../ColorFilter';
 import { colors } from '../../utils/color';
 
 describe('ColorFilter Component Appearance', () => {
-  test('There are as many buttons as color types', async () => {
+  test('There are as many checkboxes as color types', async () => {
     render(<ColorFilter />);
 
-    const buttons = await screen.findAllByRole('button');
-    expect(buttons.length).toBe(colors.length);
+    const checkboxes = await screen.findAllByRole('checkbox');
+    expect(checkboxes.length).toBe(colors.length);
   });
-});
 
-describe('ColorFilter Component Behavior', () => {
-  test('The clicked button turns disable', async () => {
+  test('The checkbox turns checked', async () => {
     const user = userEvent.setup();
     render(<ColorFilter />);
 
-    const buttons = await screen.findAllByRole('button');
+    const checkboxes = await screen.findAllByRole('checkbox');
 
-    const target = buttons[0];
-    expect(target).not.toHaveAttribute('disabled');
-    await user.click(target);
-    expect(target).toHaveAttribute('disabled');
-  });
-
-  test('The disabled button turns not disabled after clicked other one', async () => {
-    const user = userEvent.setup();
-    render(<ColorFilter />);
-
-    const buttons = await screen.findAllByRole('button');
-
-    const target = buttons[0];
-    const other = buttons[1];
-
-    await user.click(target);
-    expect(target).toHaveAttribute('disabled');
-
-    await user.click(other);
-    expect(other).toHaveAttribute('disabled');
-    expect(target).not.toHaveAttribute('disabled');
+    checkboxes.forEach(async target => {
+      expect(target).not.toBeChecked();
+      await user.click(target);
+      expect(target).toBeChecked();
+    });
   });
 });
+
+describe('ColorFilter Component Behavior', () => {});
