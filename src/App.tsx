04fe64f0cdae40d370f1470e7ed143ebd75287cf
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-02-14 03:48:04 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Grid, Container } from '@mui/material';
+import TodoHeader from './components/TodoHeader';
 import './App.css';
 
-const App = () => <div className="App">Hello</div>;
+const App = () => (
+  <Container fixed>
+    <Grid
+      container
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+      spacing={1}
+    >
+      <Grid item>
+        <TodoHeader />
+      </Grid>
+    </Grid>
+  </Container>
+);
 
 export default App;
