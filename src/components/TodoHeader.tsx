a6178b82762c8b9d1df2d81fa989c6ec000f077a
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-02-14 02:25:34 +0900

    Create TodoHeader.tsx

---
+++ src/components/TodoHeader.tsx
@@ -0,0 +1,25 @@
+import { Box, Stack } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import ColorFilter from './ColorFilter';
+import NewTodo from './NewTodo';
+import StatusFilter from './StatusFilter';
+
+const Item = styled(Box)(() => ({}));
+
+const TodoHeader = () => (
+  <Stack direction="column" spacing={1} alignItems="center">
+    <Item>
+      <NewTodo />
+    </Item>
+
+    <Item>
+      <ColorFilter />
+    </Item>
+
+    <Item>
+      <StatusFilter />
+    </Item>
+  </Stack>
+);
+
+export default TodoHeader;
