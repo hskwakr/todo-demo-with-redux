31d37af645eaf70efc9b303aa7aba19947f98493
akira <33633391+hskwakr@users.noreply.github.com>
2023-02-14 07:53:11 +0900

    Todofilter component 2 (#48)
    * Update ColorFilter.tsx
    
    * Create TodoHeader.tsx
    
    * Update App.tsx
    
    * Update TodoHeader.tsx
    
    * Update NewTodo.tsx

--- src/components/ColorFilter.tsx
+++ src/components/ColorFilter.tsx
@@ -1,23 +1,20 @@
-import { useState } from 'react';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import ColorBox from './ColorBox';
-import { ColorType, colors } from '../utils/color';
+import { Checkbox, Grid } from '@mui/material';
+import { colors } from '../utils/color';
 
 const ColorFilter = () => {
-  const [color, setColor] = useState<ColorType | null>(null);
-
-  const renderedItems = colors.map(c => {
-    const selected = color === c;
-
-    return (
-      <Grid item key={c}>
-        <Button disabled={selected} onClick={() => setColor(c)}>
-          <ColorBox color={c} />
-        </Button>
-      </Grid>
-    );
-  });
+  const renderedItems = colors.map(c => (
+    <Grid item key={c}>
+      <Checkbox
+        inputProps={{ 'aria-label': 'color-filter' }}
+        sx={{
+          color: c,
+          '&.Mui-checked': {
+            color: c,
+          },
+        }}
+      />
+    </Grid>
+  ));
 
   return (
     <Grid container spacing={2}>
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,5 +1,41 @@
-import { Input } from '@mui/material';
+import { useState } from 'react';
+import { Grid, Input } from '@mui/material';
+import ColorSwitch from './ColorSwitch';
+import { ColorType } from '../utils/color';
 
-const NewTodo = () => <Input placeholder="Enter TODO..." />;
+const NewTodo = () => {
+  const [color, setColor] = useState<ColorType>('gray');
+  const [text, setText] = useState('');
+
+  return (
+    <Grid container alignItems="center">
+      <Grid item>
+        <ColorSwitch
+          initColor={color}
+          onColorUpdated={c => {
+            setColor(c);
+          }}
+        />
+      </Grid>
+
+      <Grid item>
+        <Input
+          placeholder="Enter TODO..."
+          value={text}
+          onChange={e => {
+            setText(e.target.value);
+          }}
+          onKeyDown={e => {
+            const trimmed = e.currentTarget.value.trim();
+
+            if (e.key === 'Enter' && trimmed) {
+              setText('');
+            }
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
+};
 
 export default NewTodo;
