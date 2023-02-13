5620fe96ed3bdbc0ca5d91b8e512eb5e580bea06
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-02-14 04:00:24 +0900

    Update TodoHeader.tsx

--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -8,7 +8,7 @@ const Item = styled(Box)(() => ({}));
 
 const TodoHeader = () => (
   <Stack direction="column" spacing={1} alignItems="center">
-    <Item>
+    <Item mb={3}>
       <NewTodo />
     </Item>
 
